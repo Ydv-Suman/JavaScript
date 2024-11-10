1b396a6fdd498a2b30207eb34c814f554fc43490
Suman Yadav <sumanyadav@Sumans-MacBook-Pro.local>
2024-11-10 12:32:23 -0600

    next commit

--- object/number-method.js
+++ object/number-method.js
@@ -6,4 +6,17 @@ console.log(Math.ceil(number));
 
 let min = 10;
 let max = 20;
-console.log(Math.floor(Math.random() * (max - min)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+//
+let guess = function (num) {
+  let minimum = 1;
+  let maximum = 10;
+  let randNum = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+  console.log(`\nRandom Number: ${randNum}`);
+  if (randNum == num) {
+    return true;
+  } else return false;
+};
+let makeGuess = guess(2);
+console.log(makeGuess);
