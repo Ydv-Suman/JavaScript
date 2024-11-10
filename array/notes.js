83435a5663b322da7845c89462da98e276fe88ee
Ydv-Suman <sumanydv615@gmail.com>
2024-11-10 14:33:18 -0600

    another

---
+++ array/notes.js
@@ -0,0 +1,4 @@
+const notes = ["note 1", "note 2", "note 3"]; // we can as many as we want // not needed to be of same data type
+console.log(notes);
+console.log(notes.length); // remember length is property so no ()
+console.log(notes[notes.length - 1]); // index start at 0;
---
+++ array/todo.js
@@ -0,0 +1,3 @@
+const todo = ["Code", "Video Game", "Appointment", "Movie"];
+console.log(`I have ${todo.length} to do lists.`);
+console.log(`The first to do is: ${todo[0]}.\nThe last to do is: ${todo[todo.length-1]}.`);
