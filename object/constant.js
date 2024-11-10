fed6ebe51209b753f55e87dfce1c3c86f757fc58
Suman Yadav <sumanyadav@Sumans-MacBook-Pro.local>
2024-11-10 12:58:24 -0600

    commit

---
+++ object/constant.js
@@ -0,0 +1,24 @@
+let isValid = true;
+isValid = false;
+console.log(isValid);
+
+/*   const isFound = true;   // cont can't be reassigned
+    isFound = false;
+    console.log(isFound);   // TypeError
+ */
+
+const gradeCalc = function (obtained, total) {
+  const percentage = (obtained / total) * 100;
+  let grade = "";
+  if (percentage >= 90) {
+    return (grade = "A");
+  } else if (percentage >= 80) {
+    return (grade = "B");
+  } else if (percentage >= 70) {
+    return (grade = "C");
+  } else if (percentage >= 60) {
+    return (grade = "D");
+  } else return (grade = "F");
+};
+const data = gradeCalc(15, 20);
+console.log(data);
--- object/object-Function.js
+++ object/object-Function.js
@@ -1,56 +1,58 @@
-let myComputer = {                            // 1st Object              
-    manufacture: 'Apple',
-    model: 'Pro',
-    processor: 'M3',
-    year: 2024
-}
-let yourComputer = {                           // 2nd Objects
-    manufacture: 'Dell',
-    model: 'XPS',
-    processor: 'i9',
-    year: 2024
-}
+let myComputer = {
+  // 1st Object
+  manufacture: "Apple",
+  model: "Pro",
+  processor: "M3",
+  year: 2024,
+};
+let yourComputer = {
+  // 2nd Objects
+  manufacture: "Dell",
+  model: "XPS",
+  processor: "i9",
+  year: 2024,
+};
 
 // making function
-let computerInfo = function(computer){
-    console.log(`${computer.manufacture} has produced new ${computer.model} model with ${computer.processor} processor in ${computer.year}.`);
-}
+let computerInfo = function (computer) {
+  console.log(
+    `${computer.manufacture} has produced new ${computer.model} model with ${computer.processor} processor in ${computer.year}.`
+  );
+};
 computerInfo(myComputer);
 computerInfo(yourComputer);
-
 
 // another
 // temperature converter
 
-let tempCOnvertor = function (fahrenheit){
-
-    return {
-        fahrenheit: fahrenheit,
-        celsius: (fahrenheit-32)*5/9,
-        kelvin: ((fahrenheit-32)*5/9)+273.15
-    }
-}
+let tempCOnvertor = function (fahrenheit) {
+  return {
+    fahrenheit: fahrenheit,
+    celsius: ((fahrenheit - 32) * 5) / 9,
+    kelvin: ((fahrenheit - 32) * 5) / 9 + 273.15,
+  };
+};
 let temp = tempCOnvertor(32);
 console.log(temp);
 
 //
 let student = {
-    name: "Suman Yadav",
-    cwid: 30158478,
-    major: "CS",
-    classification: "sophomore"
-  }
+  name: "Suman Yadav",
+  cwid: 30158478,
+  major: "CS",
+  classification: "sophomore",
+};
 
-  let nextStudent = {
-    name: "Aman Bhagat",
-    cwid: 30163553,
-    major: "EEE",
-    classification: "junior"
-  }
-let studentDetails = function(stu){
-    let result = (`${stu.name} whose cwid is ${stu.cwid} is ${stu.major } major, ${stu.classification} student.       `);
-    return result;
-}
+let nextStudent = {
+  name: "Aman Bhagat",
+  cwid: 30163553,
+  major: "EEE",
+  classification: "junior",
+};
+let studentDetails = function (stu) {
+  let result = `${stu.name} whose cwid is ${stu.cwid} is ${stu.major} major, ${stu.classification} student.       `;
+  return result;
+};
 let stuDetails = studentDetails(student);
 console.log(stuDetails);
 let nextstuDetails = studentDetails(nextStudent);
