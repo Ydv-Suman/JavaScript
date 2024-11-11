02b1fdab786be2cb899147a75223df5b69faf365
Ydv-Suman <sumanydv615@gmail.com>
2024-11-11 02:10:21 -0600

    next

--- array/notes.js
+++ array/notes.js
@@ -1,28 +1,60 @@
-const notes = ["note 1", "note 2", "note 3"]; // we can as many as we want // not needed to be of same data type
-notes.forEach(function(item, index){
-    console.log(`the index is ${index} & item is ${item}`);
-})
+const notes = ["note 1", "note 2", "note 3"]; // we can as many as we want // not needed to be of same data type/
 
+// console.log('\n');
+// console.log(notes);
+// console.log(notes.length); // remember length is property so no () //// index start at 0;
 
-console.log('\n');
-console.log(notes);
-console.log(notes.length); // remember length is property so no () //// index start at 0;
-
-// methods
-notes.push("added note");   // push add value at the end
-console.log(notes);
-console.log(notes.length);
+// // methods
+// notes.push("added note");   // push add value at the end
+// console.log(notes);
+// console.log(notes.length);
   
-console.log(notes.pop()); // pop add value at the end
-console.log(notes);
-console.log(notes.length);
+// console.log(notes.pop()); // pop add value at the end
+// console.log(notes);
+// console.log(notes.length);
 
-console.log(notes.shift());  // shift method remove very first item
-notes.unshift("new first note")  // unshift method add at index 0
-console.log(`${notes}\n`)
+// console.log(notes.shift());  // shift method remove very first item
+// notes.unshift("new first note")  // unshift method add at index 0
+// console.log(`${notes}\n`)
 
-notes.splice(1, 0, "new note");  // starting index, number of item to be removed, add item if required(add at at starting index)
-console.log(notes);
+// notes.splice(1, 0, "new note");  // starting index, number of item to be removed, add item if required(add at at starting index)
+// console.log(notes);
 
-notes[0] = "new assigned at first";
-console.log(notes);
+// notes[0] = "new assigned at first";
+// console.log(notes);
+
+
+
+// //forEach
+// console.log('\n');
+// notes.forEach(function(item, index){
+//     console.log(`the index is ${index} & item is ${item}`);
+// })
+
+
+
+// // for loop
+// for (let i = 0; i < notes.length; i++){
+//     console.log(notes[i]);
+// }
+
+
+//console.log(notes.indexOf('note 1'));  // -1 if not found in array
+
+
+const newNote = [{
+    topic: 'call',
+    descp: 'friend'
+},{
+    topic: 'fitness',
+    descp: 'exercise'
+},{
+    topic: 'read',
+    descp: 'book'
+}]
+const index = newNote.findIndex(function(note, num){
+    console.log(note);  // stops immediately it find the index 
+    return note.topic === 'call';
+    // return note.descp === 'exercise';
+})
+console.log(index);
--- array/todo.js
+++ array/todo.js
@@ -1,23 +1,32 @@
 const todo = ["Exercise", "Code", "Video Game", "Appointment", "Movie"];
-todo.forEach(function(value, num){
-    console.log(`${num+1}. ${value}`);
-    // if (num == 1 || num == 2){
-    //     console.log(`${num}. ${value}`);
-    // }
-})
+
+// // // Delete the 3rd item
+// // // add a new onto the end
+// // // remove th first item from the list
+// console.log('\n');
+// console.log(todo.splice(2, 1));
+// console.log(todo);
+// todo.push("Buy food");
+// console.log(todo);
+// console.log(todo.shift());
+// console.log(todo);
+// console.log('\n');
+// console.log(`I have ${todo.length} to do lists.`);
+// console.log(`The first to do is: ${todo[0]}.\nThe last to do is: ${todo[todo.length-1]}.`);
+
+
+// console.log('\n');
+// // forEach
+// todo.forEach(function(value, num){
+//     console.log(`${num+1}. ${value}`);
+//     // if (num == 1 || num == 2){
+//     //     console.log(`${num}. ${value}`);
+//     // }
+// })
 
 
 
-// // Delete the 3rd item
-// // add a new onto the end
-// // remove th first item from the list
-console.log('\n');
-console.log(todo.splice(2, 1));
-console.log(todo);
-todo.push("Buy food");
-console.log(todo);
-console.log(todo.shift());
-console.log(todo);
-console.log('\n');
-console.log(`I have ${todo.length} to do lists.`);
-console.log(`The first to do is: ${todo[0]}.\nThe last to do is: ${todo[todo.length-1]}.`);
+// for loop
+for (let i = 0; i < todo.length; i++){
+    console.log(`${i+1}. ${todo[i]}`);
+}
