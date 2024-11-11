4240d6274ff7a161387dec0a071546781ef8b98b
Ydv-Suman <sumanydv615@gmail.com>
2024-11-11 00:47:54 -0600

    11th nov

--- array/notes.js
+++ array/notes.js
@@ -1,4 +1,21 @@
 const notes = ["note 1", "note 2", "note 3"]; // we can as many as we want // not needed to be of same data type
 console.log(notes);
-console.log(notes.length); // remember length is property so no ()
-console.log(notes[notes.length - 1]); // index start at 0;
+console.log(notes.length); // remember length is property so no () //// index start at 0;
+
+notes.push("added note");   // push add value at the end
+console.log(notes);
+console.log(notes.length);
+  
+console.log(notes.pop()); // pop add value at the end
+console.log(notes);
+console.log(notes.length);
+
+console.log(notes.shift());  // shift method remove very first item
+notes.unshift("new first note")  // unshift method add at index 0
+console.log(`${notes}\n`)
+
+notes.splice(1, 0, "new note");  // starting index, number of item to be removed, add item if required(add at at starting index)
+console.log(notes);
+
+notes[0] = "new assigned at first";
+console.log(notes);
--- array/todo.js
+++ array/todo.js
@@ -1,3 +1,13 @@
-const todo = ["Code", "Video Game", "Appointment", "Movie"];
+const todo = ["Exercise", "Code", "Video Game", "Appointment", "Movie"];
+// Delete the 3rd item
+// add a new onto the end
+// remove th first item from the list
+console.log(todo.splice(2, 1));
+console.log(todo);
+todo.push("Buy food");
+console.log(todo);
+console.log(todo.shift());
+console.log(todo);
+console.log('\n');
 console.log(`I have ${todo.length} to do lists.`);
 console.log(`The first to do is: ${todo[0]}.\nThe last to do is: ${todo[todo.length-1]}.`);
