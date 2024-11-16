526ae00ad49ba52510dee8570755d577e5de9a95
Ydv-Suman <sumanydv615@gmail.com>
2024-11-16 13:32:35 -0600

    adding button

--- note-app/notes-app.js
+++ note-app/notes-app.js
@@ -1,3 +1,14 @@
+const note = [{
+    topic: 'call',
+    descp: 'friend'
+},{
+    topic: 'fitness',
+    descp: 'exercise'
+},{
+    topic: 'read',
+    descp: 'book'
+}]
+
 // DOM document object Model
 
 // query and remove
@@ -5,12 +16,18 @@
 // p.remove()
 
 // query all and remove 
-const allP = document.querySelectorAll('p')
-allP.forEach(function(p){
-    p.remove()
-})
+// const allP = document.querySelectorAll('p')
+// allP.forEach(function(p){
+//     p.remove()
+// })
 
 // add a new element
-const addParagraph = document.createElement('p')
-addParagraph.textContent = 'This a update using JS'
-document.querySelector('body').appendChild(addParagraph)
+// const addParagraph = document.createElement('p')
+// addParagraph.textContent = 'This a update using JS'
+// document.querySelector('body').appendChild(addParagraph)
+
+// accessing the button
+
+document.querySelector('button').addEventListener('click', function(){
+    console.log('working')
+})
